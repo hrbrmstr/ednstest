db306efad7c6c3782b68a7a7d47cb0c825421aec
boB Rudis <bob@rud.is>
2019-01-10 06:29:34 -0500

    R package repo initialization complete

---
+++ R/ednstest-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ednstest>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ednstest/issues>
+#' 
+#' @md
+#' @name ednstest
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
